0e9dd045dc92d9da919dfef4044711c17a6fbad6
Dariel Jimenez <darieljimenez326@gmail.com>
2026-07-30 21:17:15 -0400

    Agregar formulario para crear historias con validacion

--- js/app.js
+++ js/app.js
@@ -1,4 +1,3 @@
-// Cada feature inicializará su propia vista aquí
 document.addEventListener('DOMContentLoaded', () => {
-  console.log('App de Relatos iniciada');
+  inicializarFormularioCreacion();
 });
